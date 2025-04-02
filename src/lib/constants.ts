bd01fe8a3eece4b1994268db01dba910125c0307
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-04-02 04:25:39 -0400

    get access token

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const CLIENT_ID = process.env.NEXT_PUBLIC_CALENDLY_CLIENT_ID;
+export const CLIENT_SECRET = process.env.NEXT_PUBLIC_CALENDLY_CLIENT_SECRET;
+export const REDIRECT_URI = process.env.NEXT_PUBLIC_CALENDLY_REDIRECT_URI;
